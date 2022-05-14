465018e9df48b2157f1da44bd47916bb45091851
Elgun8195 <tu741rmps@code.edu.az>
2022-05-14 13:35:58 +0400

    Created File

---
+++ 14.05.2022/Models/Weapon.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _14._05._2022.Models
+{
+    internal class Weapon
+    {
+        private int _capacity;
+        private int _gun;
+
+        public int Capacity
+        {
+            get { return _capacity; }
+            set
+            {
+                if (value>0)
+                {
+                    _capacity = value;
+                }
+            }
+        }
+        public int CurrentGun
+        {
+            get { return _gun; }
+            set
+            {
+                if (value>=0&&value<=Capacity)
+                {
+                    _gun = value;
+                }
+            } }
+        public bool IsSingle { get; set; } = true;
+
+
+        public Weapon(int capacity,int currentgun,bool isSingle)
+        {
+            Capacity = capacity;
+            CurrentGun = currentgun;
+            IsSingle = isSingle;
+        }
+
+
+        public void Shot()
+        {
+            if (CurrentGun==0)
+            {
+                Console.WriteLine("Daraq Bosdur");
+            }
+            else
+            {
+                CurrentGun--;
+                Console.WriteLine("Ates oldu" +", daraqda "+ CurrentGun +" gulle var");
+            }
+        }
+
+        public void Fire()
+        {
+            if (CurrentGun == 0) Console.WriteLine("Daraq bosdur");
+
+            else if(IsSingle==false)
+            {
+                int result=CurrentGun-CurrentGun;
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Daraq tekdedir deye avtomatik ates olmur");
+            }
+
+        }
+
+        public int GetRemainBulletCount()
+        {                           
+            return Capacity - CurrentGun;
+        }
+
+        public int Reload()
+        {
+            int result = Capacity - CurrentGun;
+            CurrentGun += result;
+            return CurrentGun;
+        }
+        public bool ChangeFireMode()
+        {
+            if (IsSingle)
+            {
+                return IsSingle = false;
+            }
+            else
+            {
+               return IsSingle = true;
+            }
+        }
+    }
+}
---
+++ 14.05.2022/Program.cs
@@ -0,0 +1,78 @@
+﻿using _14._05._2022.Models;
+using System;
+
+namespace _14._05._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacity;
+            int currentgun;
+            bool isSingle;
+            //Weapon weapon = new Weapon(30, 22, false);
+
+            do
+            {
+                Console.WriteLine("Daraq tutumunu yazin ");
+                capacity = Convert.ToInt32(Console.ReadLine());
+
+            } while (capacity < 0);
+            do
+            {
+                Console.WriteLine("Currentgun yazin ");
+                currentgun = Convert.ToInt32(Console.ReadLine());
+            } while (currentgun < 0 || currentgun > capacity);
+
+            Console.WriteLine("ates modunu yazin ");
+            isSingle = Convert.ToBoolean(Console.ReadLine());
+
+            Weapon weapon2 = new Weapon(capacity, currentgun, isSingle);
+
+            bool check = true;
+            do
+            {
+                Console.WriteLine("Enter The Case:");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("0. Informasiya al");
+                        Console.WriteLine("1. Shot");
+                        Console.WriteLine("2. Fire");
+                        Console.WriteLine("3. GetREmainBulletCount");
+                        Console.WriteLine("4. Reload");
+                        Console.WriteLine("5. Change Fire Mode");
+                        Console.WriteLine("6. Quit");
+                        break;
+                    case 1:
+                        weapon2.Shot();
+                        break;
+                    case 2:
+                        weapon2.Fire();
+                        break;
+
+                    case 3:
+                        Console.WriteLine(weapon2.GetRemainBulletCount());
+                        break;
+
+                    case 4:
+                        Console.WriteLine(weapon2.Reload());
+                        break;
+
+                    case 5:
+                        Console.WriteLine(weapon2.ChangeFireMode());
+                        break;
+
+                    case 6:
+                        check = false;
+                        break;
+                    default:
+                        Console.WriteLine("Qaqam Yorma Ozunu Basqa Kod Yoxdu");
+                        break;
+                }
+            } while (check);
+        }
+    }
+}
